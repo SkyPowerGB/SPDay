b351b7589bd3816e89da45acb3cacba57b219c97
SkyPowerGb <pyrobrunogb@gmail.com>
2025-02-28 06:08:18 +0100

    dodan ostatk biblioteka

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 const express =require("express");
 const app=express();
+app.set('view engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.listen("3000",()=>{console.log("server begin: http://localhost:3000")});

6400c3c125deecce9a18fdb9fdc0346714e57ff8
SkyPowerGb <pyrobrunogb@gmail.com>
2025-03-01 00:23:29 +0100

    zapoceta izrada logina

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express =require("express");
+const path = require('path');
 const app=express();
 app.set('view engine','ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
+app.use(express.static(path.join(__dirname, '..', 'public'))); 
+app.get("/",(req,res)=>{
+    res.redirect('/autentification/login.html');
+});
 app.listen("3000",()=>{console.log("server begin: http://localhost:3000")});

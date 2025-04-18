ed41d8a64057f90fa9b86157246402e6f184e96b
SkyPowerGb <pyrobrunogb@gmail.com>
2025-04-18 17:27:52 +0200

    making fin acc edit work p1

--- public/financial/scriptParts/finAccViewV2/Parts/editAccountForm.js
+++ public/financial/scriptParts/finAccViewV2/Parts/editAccountForm.js
@@ -4,15 +4,21 @@ const deleteCaseE="deleteFinAcc";
 // elements to be filled
 // finAccEditForm
 const formPartsIds={
-    idInputId:"finAccEditFormIdInput",
+    idInputId:"finAccEditFormAccIdInput",
     nameInputId:"finAccEditFormNameInput",
-    typeInputId:"finAccEditFormTypeInput",   
+    typeInputId:"finAccEditFormAccType",   
     currencyInputId:"finAccEditFormCurrencyInput",
-    balanceInputId:"finAccEditFormBalanceInput", 
+    balanceInputId:"finAccEditFormBalance", 
 }
 
-// table / displaied data (need id for item)
-const dataHoldingItemIds={}
+
+const dataHoldingItemIds={
+    idData:"finAccIdDatDispInput",
+    nameData:"finAccNameDatDisp",
+    accType:"finAccAccTypeDatDispInput",
+    currency:"finAccCurrencyDatDispInput",
+    balance:"finAccBalanceDatDispInput",
+}
 
 
 
@@ -36,7 +42,26 @@ const hiddenClass="hidden";
 
 
 function fillFormEditV(id){
+    const idInput=document.getElementById(formPartsIds.idInputId);
+    const nameInput=document.getElementById(formPartsIds.nameInputId);
+    const typeInput=document.getElementById(formPartsIds.typeInputId);
+    const currencyInput=document.getElementById(formPartsIds.currencyInputId);
+    const balanceInput=document.getElementById(formPartsIds.balanceInputId);
 
+   // const idData=document.getElementById(dataHoldingItemIds.idData);
+    const nameData=document.getElementById(dataHoldingItemIds.nameData);
+    const accType=document.getElementById(dataHoldingItemIds.accType); 
+    const currency=document.getElementById(dataHoldingItemIds.currency);
+    const balance=document.getElementById(dataHoldingItemIds.balance);
+
+    idInput.value=id;
+    nameInput.value=nameData.innerText.trim();
+    typeInput.value=accType.value;
+    currencyInput.value=currency.value;
+    balanceInput.value=balance.value;
+
+
+    
 }
 function showForm(id){
     fillFormEditV(id);
